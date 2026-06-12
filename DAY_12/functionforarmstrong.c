31167b034e6477c54347a8185d888f36df47683a
AKASH KUMAR SINGH <akashforjeem2004@gmail.com>
2026-06-12 20:57:28 +0530

    DAY-12

---
+++ DAY_12/functionforarmstrong.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int isArmstrong(int n)
+{
+    int original = n, digit, sum = 0;
+
+    while(n > 0)
+    {
+        digit = n % 10;
+        sum = sum + digit * digit * digit;
+        n = n / 10;
+    }
+
+    if(sum == original)
+        return 1;
+    else
+        return 0;
+}
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if(isArmstrong(num))
+        printf("Armstrong Number");
+    else
+        printf("Not an Armstrong Number");
+
+    return 0;
+}
---
+++ DAY_12/functionforpalindrome.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int isPalindrome(int n)
+{
+    int original = n, reverse = 0, digit;
+
+    while(n > 0)
+    {
+        digit = n % 10;
+        reverse = reverse * 10 + digit;
+        n = n / 10;
+    }
+
+    if(original == reverse)
+        return 1;
+    else
+        return 0;
+}
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if(isPalindrome(num))
+        printf("Palindrome Number");
+    else
+        printf("Not a Palindrome Number");
+
+    return 0;
+}
